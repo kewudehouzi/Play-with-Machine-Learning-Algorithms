5e683cf2afd7df2ad781ff75fe63a050ecd9bc2d
kewudehouzi <40828249+kewudehouzi@users.noreply.github.com>
2020-04-13 10:56:41 +0800

    Update LogisticRegression.py

--- 09-Logistic-Regression/04-Implement-Logistic-Regression/playML/LogisticRegression.py
+++ 09-Logistic-Regression/04-Implement-Logistic-Regression/playML/LogisticRegression.py
@@ -14,36 +14,32 @@ class LogisticRegression:
 
     def fit(self, X_train, y_train, eta=0.01, n_iters=1e4):
         """根据训练数据集X_train, y_train, 使用梯度下降法训练Logistic Regression模型"""
-        assert X_train.shape[0] == y_train.shape[0], \
-            "the size of X_train must be equal to the size of y_train"
-
-        def J(theta, X_b, y):
+        
+        def J(theta, X_b, y):#代价函数
             y_hat = self._sigmoid(X_b.dot(theta))
             try:
-                return - np.sum(y*np.log(y_hat) + (1-y)*np.log(1-y_hat)) / len(y)
+                return - np.sum(y*np.log(y_hat) + (1-y)*np.log(1-y_hat)) / len(y) #sum函数是把向量的每个维度加起来
             except:
                 return float('inf')
 
-        def dJ(theta, X_b, y):
+            
+        def dJ(theta, X_b, y):#梯度函数
             return X_b.T.dot(self._sigmoid(X_b.dot(theta)) - y) / len(y)
 
+        
         def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
-
             theta = initial_theta
             cur_iter = 0
-
             while cur_iter < n_iters:
                 gradient = dJ(theta, X_b, y)
                 last_theta = theta
                 theta = theta - eta * gradient
                 if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
                     break
-
                 cur_iter += 1
-
             return theta
 
-        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])#构造x_b矩阵，在前面加一列1
         initial_theta = np.zeros(X_b.shape[1])
         self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters)
 
@@ -52,25 +48,17 @@ class LogisticRegression:
 
         return self
 
+    
     def predict_proba(self, X_predict):
         """给定待预测数据集X_predict，返回表示X_predict的结果概率向量"""
-        assert self.intercept_ is not None and self.coef_ is not None, \
-            "must fit before predict!"
-        assert X_predict.shape[1] == len(self.coef_), \
-            "the feature number of X_predict must be equal to X_train"
-
         X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
         return self._sigmoid(X_b.dot(self._theta))
 
+    
     def predict(self, X_predict):
-        """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
-        assert self.intercept_ is not None and self.coef_ is not None, \
-            "must fit before predict!"
-        assert X_predict.shape[1] == len(self.coef_), \
-            "the feature number of X_predict must be equal to X_train"
-
+        """给定待预测数据集X_predict，返回表示X_predict的结果向量""
         proba = self.predict_proba(X_predict)
-        return np.array(proba >= 0.5, dtype='int')
+        return np.array(proba >= 0.5, dtype='int')#向量中大于0.5的元素表示为1，小于0.5的元素表示为0
 
     def score(self, X_test, y_test):
         """根据测试数据集 X_test 和 y_test 确定当前模型的准确度"""
